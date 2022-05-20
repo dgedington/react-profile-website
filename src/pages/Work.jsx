f173549dd9a32ef95d043fc9e704f34835848e0e
Daniel Edington <dgeding@live.com>
2022-05-20 14:38:34 -0500

    Added more content

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import weatherImg from '../assets/Weather_app.png';
+import taskAppImg from '../assets/task_manager_app.png';
 import underConstruct from '../assets/2489360.jpg';
 
 const Work = () => {
@@ -24,7 +25,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                Node JS Application
+                Node JS Weather App
               </span>
               <div className='pt-8 text-center'>
                 <a href='https://dgeding-weather-application.herokuapp.com/'>
@@ -41,21 +42,21 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${underConstruct})` }}
+            style={{ backgroundImage: `url(${taskAppImg})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                Some Application
+                React JS Task Manager 
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://dgeding-task-manager-frontend.herokuapp.com/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/dgedington/task-manager-frontend'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
